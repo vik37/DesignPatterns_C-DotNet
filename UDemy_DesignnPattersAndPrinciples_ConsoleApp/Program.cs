eb9bf7e536080b23dbc06bdcdafcba9f8d7acd4a
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-02 17:09:12 +0200

    Add project files.

---
+++ UDemy_DesignnPattersAndPrinciples_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDemy_DesignnPattersAndPrinciples_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
