2b4a5507afda14551c11dc562586032c39f19285
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-03 19:35:47 +0200

    Liskov Sabstitution Principle

---
+++ UDemy_DesignnPattersAndPrinciples_ConsoleApp/Liskov/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UDemy_DesignnPattersAndPrinciples_ConsoleApp.Liskov
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(this.Width)}: {this.Width}, {nameof(this.Height)}: {this.Height}";
+        }
+    }
+}
