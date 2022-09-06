4559cac74d760e98059e5ae559e5757afd1683d5
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-06 22:40:28 +0200

    Fluent Builder Inheritance with Recursive Generics.

---
+++ BuilderDP_UdemyDesignPatterns_ConsoleApp/PersonBuilder/PersonJobBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderDP_UdemyDesignPatterns_ConsoleApp.PersonBuilder
+{
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> 
+        where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorskAsA(string position)
+        {
+            person.Position = position;
+            return (SELF)this;
+        }
+    }
+}
