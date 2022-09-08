319d3f99d896b5c1758edecefcaa148573ea061c
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-09 01:04:17 +0200

    Faceted Builder DP.

---
+++ BuilderDP_UdemyDesignPatterns_ConsoleApp/FacetedBuilder/EmployeeJobBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderDP_UdemyDesignPatterns_ConsoleApp.FacetedBuilder
+{
+    public class EmployeeJobBuilder : EmployeeBuilder
+    {
+        public EmployeeJobBuilder(Employee employee)
+        {
+            this.employee = employee;
+        }
+        public EmployeeJobBuilder At(string companyName)
+        {
+            employee.CompanyName = companyName;
+            return this;
+        }
+        public EmployeeJobBuilder AsA(string position)
+        {
+            employee.Position = position;
+            return this;
+        }
+        public EmployeeJobBuilder Earning(int ammount)
+        {
+            employee.AnnualIncome = ammount;
+            return this;
+        }
+    }
+}
