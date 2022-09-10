9c2ec12601f585f0d3cce14a6604725a9004e5cc
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-10 22:29:44 +0200

    Factory Method Pattern.

---
+++ FactrotyDP_UdemyDesigPatters_CosoleApp/PointExample/Point.cs
@@ -0,0 +1,31 @@
+﻿using FactrotyDP_UdemyDesigPatters_CosoleApp.PointExample.enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactrotyDP_UdemyDesigPatters_CosoleApp.PointExample
+{
+    public class Point
+    {
+        private double x, y;
+        
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        // factory method design patern
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+        public override string ToString()
+        {
+            return $"{nameof(x)} = {x}\n{nameof(y)} = {y}";
+        }
+    }
+}
---
+++ FactrotyDP_UdemyDesigPatters_CosoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using FactrotyDP_UdemyDesigPatters_CosoleApp.PointExample;
+using System;
+
+namespace FactrotyDP_UdemyDesigPatters_CosoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factory Method Pattern.
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point);
+            Console.ReadLine();
+        }
+    }
+}
