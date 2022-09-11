ed7f7b3d064dfdfe91438fe96f72cdd22b325e47
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-11 18:02:42 +0200

    Exemple of asynchronous factory method dp.

---
+++ FactrotyDP_UdemyDesigPatters_CosoleApp/AsynchronousFactoryMethod/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactrotyDP_UdemyDesigPatters_CosoleApp.AsynchronousFactoryMethod
+{
+    public class Foo
+    {
+        private Foo()
+        {
+
+        }
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+}
