30315c8421ea26f15e1e5b5e91586a928aff3525
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-11 18:21:39 +0200

    Factory.

--- FactrotyDP_UdemyDesigPatters_CosoleApp/PointExample/Point.cs
+++ FactrotyDP_UdemyDesigPatters_CosoleApp/PointExample/Point.cs
@@ -9,20 +9,20 @@ namespace FactrotyDP_UdemyDesigPatters_CosoleApp.PointExample
     {
         private double x, y;
         
-        private Point(double x, double y)
+        public Point(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
         // factory method design patern
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
+        //public static Point NewCartesianPoint(double x, double y)
+        //{
+        //    return new Point(x, y);
+        //}
+        //public static Point NewPolarPoint(double rho, double theta)
+        //{
+        //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        //}
         public override string ToString()
         {
             return $"{nameof(x)} = {x}\n{nameof(y)} = {y}";
