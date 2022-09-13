6b850cb3bb45ccd90d0ae2cc180d1685a642d5d2
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-13 15:10:03 +0200

    IClonable is bad.

---
+++ PrototypeDP_UdemyDesignPatterns_ConsoleApp/IClonableIsBad/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp.IClonableIsBad
+{
+    public class Address : ICloneable
+    {
+        public string StreetName;
+        public int HouseNumber;
+        public Address(string streetName, int houseNumber)
+        {
+            if(streetName == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(streetName));
+            }
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+}
