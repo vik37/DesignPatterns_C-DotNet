87d1472213e26d00ea68047d732865f6454c800b
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-13 23:30:22 +0200

    Prototype Inheritance.

--- PrototypeDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ PrototypeDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿//using PrototypeDP_UdemyDesignPatterns_ConsoleApp.IClonableIsBad;
-using PrototypeDP_UdemyDesignPatterns_ConsoleApp.ConstructorCopy;
+//using PrototypeDP_UdemyDesignPatterns_ConsoleApp.ConstructorCopy;
+using PrototypeDP_UdemyDesignPatterns_ConsoleApp.PrototypeInheritance;
 using System;
 
 namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp
@@ -33,12 +34,32 @@ namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp
             #region
             // Explicit: Deep Copy Interface.
 
-            var john = new Person(new string[] { " Jürgen", "Klopp" },
-                                        new Address("FK Liverpool Road", 4));
-            var jane = john.DeepCopy();
-            jane.Address.HouseNumber = 23;
+            //var john = new Person(new string[] { " Jürgen", "Klopp" },
+            //                            new Address("FK Liverpool Road", 4));
+            //var jane = john.DeepCopy();
+            //jane.Address.HouseNumber = 23;
+            //Console.WriteLine(john);
+            //Console.WriteLine(jane);
+
+            #endregion
+
+            #region
+            // Prototype Inheritance.
+            var john = new Employee();
+            john.Names = new string[] { "John", "Doe" };
+            john.Address = new Address
+            {
+                HouseNumber = 123,
+                StreetName = "London Road"
+            };
+            john.Salery = 321000;
+            var copy = john.DeepCopy();
+
+            copy.Names[0] = "Smith";
+            copy.Address.HouseNumber++;
+            copy.Salery = 123000;
             Console.WriteLine(john);
-            Console.WriteLine(jane);
+            Console.WriteLine(copy);
 
             #endregion
             Console.ReadLine();
---
+++ PrototypeDP_UdemyDesignPatterns_ConsoleApp/PrototypeInheritance/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp.PrototypeInheritance
+{
+    public class Person : IDeepCopyable<Person>
+    {
+        public string[] Names;
+        public Address Address;
+        public Person() { }
+
+        public Person(string[] names, Address address)
+        {
+            if (names == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(names));
+            }
+            if (address == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address));
+            }
+            Names = names;
+            Address = address;
+        }
+
+        public virtual void CopyTo(Person target)
+        {
+            target.Names = (string[])Names.Clone();
+            target.Address = Address.DeepCopy();
+        }
+
+        //public Person DeepCopy()
+        //{
+        //    return new Person((string[])Names.Clone(), Address.DeepCopy());
+        //}
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+}
