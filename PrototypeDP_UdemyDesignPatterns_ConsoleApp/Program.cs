1c0b2f8a812d890913d28b3807b3c3f66b7acd1b
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-13 16:08:56 +0200

    Copy Constructor.

--- PrototypeDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ PrototypeDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using PrototypeDP_UdemyDesignPatterns_ConsoleApp.IClonableIsBad;
+﻿//using PrototypeDP_UdemyDesignPatterns_ConsoleApp.IClonableIsBad;
+using PrototypeDP_UdemyDesignPatterns_ConsoleApp.ConstructorCopy;
 using System;
 
 namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp
@@ -9,13 +10,22 @@ namespace PrototypeDP_UdemyDesignPatterns_ConsoleApp
         {
             #region
             // IClonable interface is bad.
-            var john = new Person(new string[] { "John", "Smith" }, new Address("London Road", 4));
-            
-
+            //var john = new Person(new string[] { "John", "Smith" }, new Address("London Road", 4));            
             //var jane = john;
             //jane.Names[0] = "Jane";
-            var jane = (Person)john.Clone();
-            jane.Address.HouseNumber = 321;
+            //var jane = (Person)john.Clone();
+            //jane.Address.HouseNumber = 321;
+            //Console.WriteLine(john);
+            //Console.WriteLine(jane);
+            #endregion
+
+            #region
+            // Constructor Copy
+
+            var john = new Person(new string[] { " Jürgen", "Klopp" }, 
+                                        new Address("FK Liverpool Road", 4));
+            var jane = new Person(john);
+            jane.Address.StreetName = "Nokshir Real Madrid.";
             Console.WriteLine(john);
             Console.WriteLine(jane);
             #endregion
