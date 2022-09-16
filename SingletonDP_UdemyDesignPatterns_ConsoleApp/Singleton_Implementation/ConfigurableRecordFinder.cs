446976197dd8ee417e56f4186f64598a0af34da1
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-16 23:02:12 +0200

    SingletonDP with testing and help of DI Autofac.

---
+++ SingletonDP_UdemyDesignPatterns_ConsoleApp/Singleton_Implementation/ConfigurableRecordFinder.cs
@@ -0,0 +1,25 @@
+﻿using SingletonDP_UdemyDesignPatterns_ConsoleApp.Singleton_Implementation.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDP_UdemyDesignPatterns_ConsoleApp.Singleton_Implementation
+{
+    public class ConfigurableRecordFinder
+    {
+        private IDatabase database;
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database ?? throw new ArgumentNullException(paramName: nameof(database));
+        }
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+            {
+                result += database.GetPopulation(name);
+            }
+            return result;
+        }
+    }
+}
