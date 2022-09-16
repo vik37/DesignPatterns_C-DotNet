0163f745760699e8014107537c90fb79ea2e1549
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-16 19:33:51 +0200

    Singleton Implementation.

---
+++ SingletonDP_UdemyDesignPatterns_ConsoleApp/Singleton_Implementation/interfaces/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDP_UdemyDesignPatterns_ConsoleApp.Singleton_Implementation.interfaces
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+}
