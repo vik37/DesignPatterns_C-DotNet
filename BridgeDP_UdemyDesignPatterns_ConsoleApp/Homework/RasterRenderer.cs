a3eb054849eed82b7c4f0ced69117a7ab128bfe4
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-21 19:38:38 +0200

    Bridge D.P. Task.

---
+++ BridgeDP_UdemyDesignPatterns_ConsoleApp/Homework/RasterRenderer.cs
@@ -0,0 +1,14 @@
+﻿using BridgeDP_UdemyDesignPatterns_ConsoleApp.Homework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDP_UdemyDesignPatterns_ConsoleApp.Homework
+{
+    public class RasterRenderer : IRenderer
+    {
+
+        public string WhatToRenderAs { get { return "Line"; } }
+        
+    }
+}
---
+++ BridgeDP_UdemyDesignPatterns_ConsoleApp/Homework/Triangle.cs
@@ -0,0 +1,12 @@
+﻿using BridgeDP_UdemyDesignPatterns_ConsoleApp.Homework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDP_UdemyDesignPatterns_ConsoleApp.Homework
+{
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer renderer) : base(renderer) => Name = "Triangle";
+    }
+}
