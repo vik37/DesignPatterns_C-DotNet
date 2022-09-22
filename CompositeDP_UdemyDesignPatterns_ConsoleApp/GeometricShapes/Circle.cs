03d9e6ce822e4560c39344f99314664fc59c4764
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-22 17:35:25 +0200

    Geometric Shapes.

---
+++ CompositeDP_UdemyDesignPatterns_ConsoleApp/GeometricShapes/Circle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeDP_UdemyDesignPatterns_ConsoleApp.GeometricShapes
+{
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+
+    }
+}
---
+++ CompositeDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using CompositeDP_UdemyDesignPatterns_ConsoleApp.GeometricShapes;
+using System;
+
+namespace CompositeDP_UdemyDesignPatterns_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            // Geometric Shapes
+            var drawing = new GraphicObject { Name = "My Drawing" };
+            drawing.Children.Add(new Circle { Color = "Red" });
+            drawing.Children.Add(new Square { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color="Blue"});
+            group.Children.Add(new Square { Color = "Blue" });
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
