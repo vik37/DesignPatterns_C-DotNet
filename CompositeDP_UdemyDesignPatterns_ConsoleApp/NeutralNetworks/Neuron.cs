d9f93e5eebc0698425cfce71a5a87857fddd8b92
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-22 20:12:52 +0200

    Neutral Network.

---
+++ CompositeDP_UdemyDesignPatterns_ConsoleApp/NeutralNetworks/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeDP_UdemyDesignPatterns_ConsoleApp.NeutralNetworks
+{
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float value;
+        public List<Neuron>  In, Out;
+        //public void ConnectTo(Neuron other)
+        //{
+        //    Out.Add(other);
+        //    other.In.Add(this);
+        //}
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CompositeDP_UdemyDesignPatterns_ConsoleApp/NeutralNetworks/NeuronLayer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CompositeDP_UdemyDesignPatterns_ConsoleApp.NeutralNetworks
+{
+    public class NeuronLayer : Collection<Neuron>
+    {
+    }
+}
