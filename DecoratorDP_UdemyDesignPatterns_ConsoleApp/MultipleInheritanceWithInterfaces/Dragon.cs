0281142649f3d28446b4703be85359eaf2712393
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-25 19:46:40 +0200

    Multiple Inheritance with Interfaces.

---
+++ DecoratorDP_UdemyDesignPatterns_ConsoleApp/MultipleInheritanceWithInterfaces/Dragon.cs
@@ -0,0 +1,33 @@
+﻿using DecoratorDP_UdemyDesignPatterns_ConsoleApp.MultipleInheritanceWithInterfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorDP_UdemyDesignPatterns_ConsoleApp.MultipleInheritanceWithInterfaces
+{
+    public class Dragon : IBird, ILizard
+    {
+        private Bird Bird = new Bird();
+        private Lizard Lizard = new Lizard();
+        private int weight;
+
+        public int Weight {
+            get { return weight; }
+            set { 
+                    weight = value; 
+                    Bird.Weight = value;
+                    Lizard.Weight = value;
+                }
+        }
+
+        public void Crowl()
+        {
+            Lizard.Crowl();
+        }
+
+        public void Fly()
+        {
+            Bird.Fly();
+        }
+    }
+}
