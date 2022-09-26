77589a44f7063b70209e3b0702b3660ea811e9eb
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-26 20:00:39 +0200

    Decorator Pattern with Dependency Injection (Autofac).

---
+++ DecoratorDP_UdemyDesignPatterns_ConsoleApp/DecoratorInDI/ReportingService.cs
@@ -0,0 +1,15 @@
+﻿using DecoratorDP_UdemyDesignPatterns_ConsoleApp.DecoratorInDI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorDP_UdemyDesignPatterns_ConsoleApp.DecoratorInDI
+{
+    public class ReportingService : IReportingService
+    {
+        public void Report()
+        {
+            Console.WriteLine("Here is your report");
+        }
+    }
+}
