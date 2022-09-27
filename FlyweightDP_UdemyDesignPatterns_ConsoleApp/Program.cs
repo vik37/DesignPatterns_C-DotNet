1ad075c0de6655137697884c855312e1138a8214
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-27 18:13:00 +0200

    Formatting Text.

--- FlyweightDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ FlyweightDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FlyweightDP_UdemyDesignPatterns_ConsoleApp.RepeatingUsername;
+using FlyweightDP_UdemyDesignPatterns_ConsoleApp.TextFormatting;
 using JetBrains.dotMemoryUnit;
 using NUnit.Framework;
 using System;
@@ -13,9 +14,15 @@ namespace FlyweightDP_UdemyDesignPatterns_ConsoleApp
         
         static void Main(string[] args)
         {
-            
-
-            
+            #region
+            // Text Formatting
+            var ft = new FormattedText("This is a brave new world");
+            ft.Capitalize(10, 15);
+            Console.WriteLine(ft);
+            var bft = new BetterFormaterText("This is a brave new world");
+            bft.GetRange(10, 15).Capitalize = true;
+            Console.WriteLine(bft);
+            #endregion
             Console.ReadLine();
         }
         #region
---
+++ FlyweightDP_UdemyDesignPatterns_ConsoleApp/TextFormatting/BetterFormaterText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyweightDP_UdemyDesignPatterns_ConsoleApp.TextFormatting
+{
+    public class BetterFormaterText
+    {
+        private string planeText;
+        private List<TextRange> formatting = new List<TextRange>();
+        public BetterFormaterText(string planeText)
+        {
+            this.planeText = planeText;
+        }
+        // Return Flyweight object
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < planeText.Length; i++)
+            {
+                var c = planeText[i];
+                foreach (var range in formatting)
+                {
+                    if(range.Covers(i) && range.Capitalize)
+                    {
+                        c = char.ToUpper(c);
+                    }
+                }
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
