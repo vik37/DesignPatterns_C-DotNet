552cff087e56daceda34326531890367b70c3d3c
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-27 14:57:54 +0200

    Facade Pattern.

---
+++ FacadeDP_UdemyDesignPatterns_ConsoleApp/Task_Homework/Facade/Generator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadeDP_UdemyDesignPatterns_ConsoleApp.Task_Homework.Facade
+{
+    //makes an array of random digits (suitably constrained) of a particular length.
+    public class Generator
+    {
+        private static readonly Random random = new Random();
+
+        public List<int> Generate(int count)
+        {
+            return Enumerable.Range(0, count)
+              .Select(_ => random.Next(1, 6))
+              .ToList();
+        }
+    }
+}
---
+++ FacadeDP_UdemyDesignPatterns_ConsoleApp/Task_Homework/Facade/Verifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadeDP_UdemyDesignPatterns_ConsoleApp.Task_Homework.Facade
+{
+    public class Verifier
+    {
+        public bool Verify(List<List<int>> array)
+        {
+            if (!array.Any()) return false;
+
+            var expected = array.First().Sum();
+
+            return array.All(t => t.Sum() == expected);
+        }
+    }
+}
