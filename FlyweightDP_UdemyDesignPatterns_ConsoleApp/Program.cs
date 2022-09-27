e37d228fdcd29799d28b32df9291a71eda8b1d50
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-27 17:36:37 +0200

    Memory Saving.

---
+++ FlyweightDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using FlyweightDP_UdemyDesignPatterns_ConsoleApp.RepeatingUsername;
+using JetBrains.dotMemoryUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlyweightDP_UdemyDesignPatterns_ConsoleApp
+{
+    [TestFixture]
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            #region
+            // Repeating User Names
+
+            #endregion
+            Console.ReadLine();
+        }
+        [Test]
+        public void TestUser()
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User>();
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User($"{firstName} {lastName}"));
+                }
+            }
+            ForseGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+        [Test]
+        public void TestUser2()
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User2>();
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User2($"{firstName} {lastName}"));
+                }
+            }
+            ForseGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+        private void ForseGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+        private string RandomString()
+        {
+            Random r = new Random();
+            return new string(
+                    Enumerable.Range(0, 10)
+                    .Select(i => (char)('a' + r.Next(26)))
+                    .ToArray());
+        }
+    }
+}
---
+++ FlyweightDP_UdemyDesignPatterns_ConsoleApp/RepeatingUsername/User2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlyweightDP_UdemyDesignPatterns_ConsoleApp.RepeatingUsername
+{
+    public class User2
+    {
+        public static List<string> strings = new List<string>();
+        private int[] names;
+        public User2(string fullName)
+        {
+            int GetOrAdd(string s)
+            {
+                int idx = strings.IndexOf(s);
+                if (idx == -1) return idx;
+                else
+                {
+                    strings.Add(s);
+                    return strings.Count - 1;
+                }                
+            }
+            names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+        }
+    }
+}
