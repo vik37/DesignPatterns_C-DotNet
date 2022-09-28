ab02938d6be2ab805b12f2f8fd90454968791c59
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-28 18:14:35 +0200

    Protection Proxy.

---
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy;
+using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy.Interfaces;
+using System;
+
+namespace ProxyDP_UdemyDesignPatterns_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            // Protection Proxy
+            ICar car = new CarProxy(new Driver(12));
+            car.Drive();
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
