9e5f99f706bdcb3ed7615d0dd9188bdf1cae73c4
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-28 19:28:13 +0200

    Property Proxy.

--- ProxyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy;
+﻿using ProxyDP_UdemyDesignPatterns_ConsoleApp.PropertyProxy;
+using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy;
 using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy.Interfaces;
 using System;
 
@@ -10,8 +11,18 @@ namespace ProxyDP_UdemyDesignPatterns_ConsoleApp
         {
             #region
             // Protection Proxy
-            ICar car = new CarProxy(new Driver(12));
-            car.Drive();
+            //ICar car = new CarProxy(new Driver(12));
+            //car.Drive();
+            #endregion
+
+            #region
+            // Property Proxy
+            var c = new Creature();
+            c.Agility = 10; // c.set_agility(10) will not work;
+                            // c.Agility = new Property<int>(10)
+
+            c.Agility = 10; 
+            // If we assign value twice, we're only actually assigning once.
             #endregion
             Console.ReadLine();
         }
---
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/PropertyProxy/Creature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyDP_UdemyDesignPatterns_ConsoleApp.PropertyProxy
+{
+    public class Creature
+    {
+        private Property<int> agility = new Property<int>();
+        public int Agility
+        {
+            get => agility.Value;
+            set => agility.Value = value;
+        }
+    }
+}
