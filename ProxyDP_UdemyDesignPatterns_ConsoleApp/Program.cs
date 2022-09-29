2359d9bf85ce7561d66bf9a3393e1b4b2f64b55b
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-29 20:32:28 +0200

    Value Proxy.

--- ProxyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ProxyDP_UdemyDesignPatterns_ConsoleApp.PropertyProxy;
 using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy;
 using ProxyDP_UdemyDesignPatterns_ConsoleApp.ProtectionProxy.Interfaces;
+using ProxyDP_UdemyDesignPatterns_ConsoleApp.ValueProxy;
 using System;
 
 namespace ProxyDP_UdemyDesignPatterns_ConsoleApp
@@ -17,14 +18,20 @@ namespace ProxyDP_UdemyDesignPatterns_ConsoleApp
 
             #region
             // Property Proxy
-            var c = new Creature();
-            c.Agility = 10; // c.set_agility(10) will not work;
-                            // c.Agility = new Property<int>(10)
+            //var c = new Creature();
+            //c.Agility = 10; // c.set_agility(10) will not work;
+            // c.Agility = new Property<int>(10)
 
-            c.Agility = 10;
+            //c.Agility = 10;
             // If we assign value twice, we're only actually assigning once.
-            c.Letter = 'A';
-            c.Letter = 'B';
+            //c.Letter = 'A';
+            //c.Letter = 'B';
+            #endregion
+
+            #region
+            // Value Proxy
+            Console.WriteLine(10f * 5.Percent());
+            Console.WriteLine(2.Percent() + 3.Percent()); //5%
             #endregion
             Console.ReadLine();
         }
