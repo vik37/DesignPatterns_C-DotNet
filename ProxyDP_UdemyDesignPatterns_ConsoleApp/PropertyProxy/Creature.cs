778d20b767caa698e8fd93e3e1d21c85e5e7f258
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-29 14:02:55 +0200

    Testing with 2 properties.

--- ProxyDP_UdemyDesignPatterns_ConsoleApp/PropertyProxy/Creature.cs
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/PropertyProxy/Creature.cs
@@ -7,10 +7,16 @@ namespace ProxyDP_UdemyDesignPatterns_ConsoleApp.PropertyProxy
     public class Creature
     {
         private Property<int> agility = new Property<int>();
+        private Property<char> letter = new Property<char>();
         public int Agility
         {
             get => agility.Value;
             set => agility.Value = value;
+        }
+        public char Letter
+        {
+            get => letter.Value;
+            set => letter.Value = value;
         }
     }
 }
