daf1c13d3f45b7c676991ec3a712d1987744daef
vik37 <59177404+vik37@users.noreply.github.com>
2022-09-30 21:06:22 +0200

    Bit Fragging.

---
+++ ProxyDP_UdemyDesignPatterns_ConsoleApp/BitFragging/TwoBitSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyDP_UdemyDesignPatterns_ConsoleApp.BitFragging
+{
+    public class TwoBitSet
+    {
+        // 64 bits --> 32 values
+        private readonly ulong data;
+        public TwoBitSet(ulong data)
+        {
+            this.data = data;
+        }
+        // 00 10 01 10
+        public byte this[int index]
+        {
+            get
+            {
+                // 00 10 01 10
+                var shift = index << 1;
+                
+                ulong mask = (0b11U << shift); // 00 11 00 00
+
+                // 00 10 00 00 >> shift
+                // 00 00 00 00 00 00 00 00 10
+                return (byte)((data & mask) >> shift);
+            }
+        }
+    }
+}
