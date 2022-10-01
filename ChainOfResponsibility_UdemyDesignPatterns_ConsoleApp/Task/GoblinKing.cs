2711ceb9fd4421e0dffd8c60eef6942ec82a4b27
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-01 23:12:52 +0200

    Task - Chain of Responsibility Design Pattern

---
+++ ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp/Task/GoblinKing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp.Task
+{
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) : base(game, 3, 3)
+        { }
+        public override void Query(object obj, StatQuery q)
+        {
+            if (!ReferenceEquals(obj, this) && q.Statistics == Statistics.Attack)
+            {
+                q.Result++; 
+            }
+            else base.Query(obj, q);
+        }
+
+    }
+}
---
+++ ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp/Task/Statistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp.Task
+{
+    public enum Statistics
+    {
+        Attack,
+        Defense
+    }
+}
