2ee08b06ca7c6afadd9bdf5a0183e43ce17651a8
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-01 20:27:37 +0200

    Broken Chain.

---
+++ ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp/BrokenChain/IncreaseModifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp.BrokenChain
+{
+    public class IncreaseModifier : CreatureModifier
+    {
+        public IncreaseModifier(Game game, Creature creature) : base(game, creature)
+        {
+        }
+
+        protected override void Handle(object sender, Query q)
+        {
+            if (q.CreatureName == creature.Name
+                && q.WhatToQuery == Query.Argument.Defence)
+            {
+                q.Value += 3;
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp/BrokenChain/Query.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility_UdemyDesignPatterns_ConsoleApp.BrokenChain
+{
+    public class Query
+    {
+        public string CreatureName;
+        public enum Argument
+        {
+            Attack, Defence
+        }
+        public Argument WhatToQuery;
+        public int Value;
+        public Query(string creatureName, Argument whatToQuery, int value)
+        {
+            CreatureName = creatureName;
+            WhatToQuery = whatToQuery;
+            Value = value;
+        }
+    }
+}
