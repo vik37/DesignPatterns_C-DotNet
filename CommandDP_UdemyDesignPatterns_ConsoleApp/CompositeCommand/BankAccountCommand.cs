1e7cb2e0a82da04a16c6fd9c9940139bf3525d45
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-02 19:53:18 +0200

    Combination: Command DP & Comsposite DP.

---
+++ CommandDP_UdemyDesignPatterns_ConsoleApp/CompositeCommand/BankAccountCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandDP_UdemyDesignPatterns_ConsoleApp.CompositeCommand
+{
+    public class BankAccountCommand : ICommand
+    {
+        private BankAccount account;
+        private Action action;
+        private int amount;
+        public BankAccountCommand(BankAccount account, Action action, int amount)
+        {
+            this.account = account;
+            this.action = action;
+            this.amount = amount;
+        }
+
+        public bool Success { get; set; }
+
+        public void Call()
+        {
+            switch (action)
+            {
+                case Action.Deposite:
+                    account.Deposite(amount);
+                    Success = true;
+                    break;
+                case Action.Withdrow:
+                    Success = account.Withdrow(amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Undo()
+        {
+            if (!Success) return;
+            switch (action)
+            {
+                case Action.Deposite:
+                    account.Withdrow(amount);
+                    break;
+                case Action.Withdrow:
+                    account.Deposite(amount);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public enum Action
+        {
+            Deposite, Withdrow
+        }
+    }
+}
