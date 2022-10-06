587305e2f49b9bd918c8bb9cda183ce41f552a8b
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-06 23:05:08 +0200

    Iterator Object.

---
+++ IteratorDP_UdemyDesignPatterns_ConsoleApp/Iterator/BinaryTree.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorDP_UdemyDesignPatterns_ConsoleApp.Iterator
+{
+    public class BinaryTree<T>
+    {
+    }
+}
---
+++ IteratorDP_UdemyDesignPatterns_ConsoleApp/Iterator/InOrderIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorDP_UdemyDesignPatterns_ConsoleApp.Iterator
+{
+    public class InOrderIterator<T>
+    {
+        private readonly Node<T> root;
+        private bool yealdedStart;
+        public Node<T> Current;
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = root;
+            Current = root;
+            while(Current.Left != null)
+            {
+                Current = Current.Left;
+            }
+        }
+        public bool MoveNext()
+        {
+            if (!yealdedStart)
+            {
+                yealdedStart = true;
+                return true;
+            }
+            if(Current.Right != null)
+            {
+                Current = Current.Right;
+                while(Current.Left != null)
+                {
+                    Current = Current.Left;                    
+                }
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while(p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+                Current = p;
+                return Current != null;
+            }
+        }
+        public void Reset()
+        {
+
+        }
+    }
+}
