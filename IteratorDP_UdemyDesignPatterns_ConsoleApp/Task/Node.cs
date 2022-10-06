4fdc126608ba2f39a67a04df40855128f0272648
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-07 01:37:46 +0200

    Task

---
+++ IteratorDP_UdemyDesignPatterns_ConsoleApp/Task/Node.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorDP_UdemyDesignPatterns_ConsoleApp.Task
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+
+        private IEnumerable<Node<T>> Traverse(Node<T> current)
+        {
+            yield return current;
+            if (current.Left != null)
+            {
+                foreach (var left in Traverse(current.Left))
+                    yield return left;
+            }
+            if (current.Right != null)
+            {
+                foreach (var right in Traverse(current.Right))
+                    yield return right;
+            }
+        }
+
+        public IEnumerable<T> PreOrder
+        {
+            get
+            {
+                foreach (var node in Traverse(this))
+                    yield return node.Value;
+            }
+        }
+    }
+}
