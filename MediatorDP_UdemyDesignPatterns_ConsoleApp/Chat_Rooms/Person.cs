578f52f4506f0bcf528e2db6116d5506265df1b6
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-07 21:13:03 +0200

    Chat Room.

---
+++ MediatorDP_UdemyDesignPatterns_ConsoleApp/Chat_Rooms/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDP_UdemyDesignPatterns_ConsoleApp.Chat_Rooms
+{
+    public class Person
+    {
+        public string Name;
+        public ChatRoom Room;
+        private List<string> chatLog = new List<string>();
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public void Say(string message)
+        {
+            Room.Broadcast(Name,message);
+        }
+        public void PrivateMessage(string who, string message)
+        {
+            Room.Message(Name,who,message);
+        }
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: {message}";
+            chatLog.Add(s);
+            Console.WriteLine($"[{Name}'s chat session] {s}");
+        }
+    }
+}
