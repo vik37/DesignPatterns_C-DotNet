3ccca188e032c61b14ebedb8159acaeedc1759b6
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-08 19:48:44 +0200

    MediatR (Nu Get Package Library).

---
+++ MediatorDP_UdemyDesignPatterns_ConsoleApp/MediatR_Demo/PingCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDP_UdemyDesignPatterns_ConsoleApp.MediatR_Demo
+{
+    public class PingCommand : IRequest<PongResponse>
+    {
+    }
+}
---
+++ MediatorDP_UdemyDesignPatterns_ConsoleApp/MediatR_Demo/PingCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatorDP_UdemyDesignPatterns_ConsoleApp.MediatR_Demo
+{
+    [UsedImplicitly]
+    public class PingCommandHandler : IRequestHandler<PingCommand, PongResponse>
+    {
+        public async Task<PongResponse> Handle(PingCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new PongResponse(DateTime.UtcNow))
+                .ConfigureAwait(false);
+        }
+    }
+}
