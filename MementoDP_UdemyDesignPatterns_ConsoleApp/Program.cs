c69a0ecd33992daf3b66e02fbdc6116e5bd907d8
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-09 22:44:14 +0200

    Undo and Redo.

--- MementoDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ MementoDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -8,17 +8,31 @@ namespace MementoDP_UdemyDesignPatterns_ConsoleApp
         static void Main(string[] args)
         {
             #region Memento
-            var ba = new BankAccount(100);
-            var m1 = ba.Deposit(50); // 150
-            var m2 = ba.Deposit(25); // 175
-            Console.WriteLine(ba);
+            //var ba = new BankAccount(100);
+            //var m1 = ba.Deposit(50); // 150
+            //var m2 = ba.Deposit(25); // 175
+            //Console.WriteLine(ba);
 
-            ba.Restore(m1);
-            Console.WriteLine(ba);
+            //ba.Restore(m1);
+            //Console.WriteLine(ba);
 
-            ba.Restore(m2);
-            Console.WriteLine(ba);
+            //ba.Restore(m2);
+            //Console.WriteLine(ba);
             #endregion
+
+            #region Undo and Redo
+            var ba = new BankAccount(100);
+            ba.Deposit(50);
+            ba.Deposit(25);
+            Console.WriteLine(ba);
+
+            ba.Undo();
+            Console.WriteLine($"1st Undo: {ba}");
+            ba.Undo();
+            Console.WriteLine($"2nd Undo: {ba}");
+            ba.Redo();
+            Console.WriteLine($"1st Redo: {ba}");
+            #endregion  
             Console.ReadLine();
         }
     }
