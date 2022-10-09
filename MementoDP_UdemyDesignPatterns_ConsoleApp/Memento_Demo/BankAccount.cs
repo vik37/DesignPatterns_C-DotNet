ef1cb24f3725ca13f0cf09ac50cb181e18e3f763
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-09 22:12:15 +0200

    Memento.

---
+++ MementoDP_UdemyDesignPatterns_ConsoleApp/Memento_Demo/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoDP_UdemyDesignPatterns_ConsoleApp.Memento_Demo
+{
+    public class BankAccount
+    {
+        private int balance;
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            return new Memento(balance);
+        }
+        public void Restore(Memento m)
+        {
+            balance = m.Balance;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
