75f959a3dab9204ae2ed75f09af4423530a3c91a
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-10 15:49:13 +0200

    Memento Test.

---
+++ MementoDP_UdemyDesignPatterns_ConsoleApp/Task/Memento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoDP_UdemyDesignPatterns_ConsoleApp.Task
+{
+    public class Memento
+    {
+        public List<Token> Tokens = new List<Token>();
+    }
+}
