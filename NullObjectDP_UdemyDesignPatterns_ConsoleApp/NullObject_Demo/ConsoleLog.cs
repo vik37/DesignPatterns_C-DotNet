11d1ce7f704acaefd3a5d1c3e178c5f0727f76a2
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-10 20:01:49 +0200

    Null Object Design Pattern.

---
+++ NullObjectDP_UdemyDesignPatterns_ConsoleApp/NullObject_Demo/ConsoleLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace NullObjectDP_UdemyDesignPatterns_ConsoleApp.NullObject_Demo
+{
+    public class ConsoleLog : ILog
+    {
+        public void Info(string msg)
+        {
+            WriteLine(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            WriteLine("WARNING!!! " + msg);
+        }
+    }
+}
---
+++ NullObjectDP_UdemyDesignPatterns_ConsoleApp/NullObject_Demo/NullLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullObjectDP_UdemyDesignPatterns_ConsoleApp.NullObject_Demo
+{
+    public class NullLog : ILog
+    {
+        public void Info(string msg)
+        {
+
+        }
+
+        public void Warn(string msg)
+        {
+
+        }
+    }
+}
