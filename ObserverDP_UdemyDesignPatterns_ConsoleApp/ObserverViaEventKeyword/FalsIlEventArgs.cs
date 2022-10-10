d76603e7881b7657f5243d231f562adb7128a56b
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-10 22:10:13 +0200

    Observer via 'event' Keyword.

---
+++ ObserverDP_UdemyDesignPatterns_ConsoleApp/ObserverViaEventKeyword/FalsIlEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverDP_UdemyDesignPatterns_ConsoleApp.ObserverViaEventKeyword
+{
+    public class FalsIlEventArgs
+    {
+        public string Address;
+    }
+}
---
+++ ObserverDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using ObserverDP_UdemyDesignPatterns_ConsoleApp.ObserverViaEventKeyword;
+using System;
+
+namespace ObserverDP_UdemyDesignPatterns_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Observer via 'event' Keyword
+            var person = new Person();
+            person.FallsIll += CallDoctor;
+            person.CatchACold();
+            person.FallsIll -= CallDoctor;
+            #endregion
+            Console.ReadLine();
+        }
+
+        #region Observer via 'event' Keyword
+        public static void CallDoctor(object sender, FalsIlEventArgs eventArgs)
+        {
+            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
+        }
+        #endregion
+    }
+}
