bd32a15c2dc494ae7a1d186445678d702368368e
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-10 22:54:19 +0200

    Weak Event Pattern.

---
+++ ObserverDP_UdemyDesignPatterns_ConsoleApp/WeakEventPattern/Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverDP_UdemyDesignPatterns_ConsoleApp.WeakEventPattern
+{
+    public class Window
+    {
+        public Window(Button button)
+        {
+            button.Clicked += ButtonClicked;
+
+            // Part of a indowsBase assembly - Dot Net Framework 4.5
+            //WeakEventManager<Button,EventArgs>
+            //    .AddHandler(button, "clicked",ButtonClicked)
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked (window handler)");
+        }
+        ~Window()
+        {
+            Console.WriteLine("Window finalized");
+        }
+    }
+}
