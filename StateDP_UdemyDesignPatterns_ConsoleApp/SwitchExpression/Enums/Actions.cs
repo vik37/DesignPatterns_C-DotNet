12e2f2bae53297ec75fbf42131639312630b44ea
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-16 21:34:40 +0200

    Switch Expression.

---
+++ StateDP_UdemyDesignPatterns_ConsoleApp/SwitchExpression/Enums/Actions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateDP_UdemyDesignPatterns_ConsoleApp.SwitchExpression.Enums
+{
+    public enum Actions
+    {
+        Open,
+        Close
+    }
+}
