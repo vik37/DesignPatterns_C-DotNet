d895b42dcd305725403a4c8faa6f55397e346c0d
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-16 23:58:24 +0200

    Task.

---
+++ StateDP_UdemyDesignPatterns_ConsoleApp/Task/CombinationLock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateDP_UdemyDesignPatterns_ConsoleApp.Task
+{
+    public class CombinationLock
+    {
+        private readonly int[] combination;
+        public string Status = "LOCKED";
+        private int count = 0;
+        public CombinationLock(int[] combination)
+        {
+            this.combination = combination;
+        }
+        public void EnterDigit(int digit)
+        {
+            if(count == 0) Status = "";
+
+            
+            if(count + 1 == combination.Length && digit == combination[count])
+            {
+                Status = "OPEN";
+            }
+            else if (digit == combination[count])
+            {
+                this.Status += digit.ToString();
+                ++count;
+            }
+            else
+            {
+                Status = "ERROR";
+            }
+        }
+    }
+}
