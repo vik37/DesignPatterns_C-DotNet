2f9f8071ee571ab37ab2341f3bc4381c025c0cb3
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-16 18:29:51 +0200

    Handmade State Machine.

---
+++ StateDP_UdemyDesignPatterns_ConsoleApp/HandmadeStateMachine/Enums/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateDP_UdemyDesignPatterns_ConsoleApp.HandmadeStateMachine.Enums
+{
+    public enum Trigger
+    {
+        CallDialed,
+        HungUp,
+        CallConnected,
+        PlaceOnHold,
+        TakenOffHold,
+        LeftMessage
+    }
+}
