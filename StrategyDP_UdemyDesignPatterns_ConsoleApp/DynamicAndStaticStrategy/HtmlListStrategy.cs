1b30103cdeb5f0ea920f6c506ddf23492ea6444d
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-17 15:14:41 +0200

    Dynamic Strategy.

---
+++ StrategyDP_UdemyDesignPatterns_ConsoleApp/DynamicAndStaticStrategy/HtmlListStrategy.cs
@@ -0,0 +1,25 @@
+﻿using StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy
+{
+    public class HtmlListStrategy : IListStrategy
+    {       
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"   <li>{item}</li>");
+        }
+
+    }
+}
--- StrategyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ StrategyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy;
+using StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy.Enums;
+using System;
 
 namespace StrategyDP_UdemyDesignPatterns_ConsoleApp
 {
@@ -6,6 +8,25 @@ namespace StrategyDP_UdemyDesignPatterns_ConsoleApp
     {
         static void Main(string[] args)
         {
+            #region Dynamic and Static Strategy
+            var tp = new TextProccessor();
+            tp.SetOutpuFormat(OutputFormat.Markdown);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+
+            tp.Clear();
+            tp.SetOutpuFormat(OutputFormat.Html);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+            #endregion
+
+            #region Equality and Comparation Strategies
+
+            #endregion
+
+            #region Task
+
+            #endregion
             Console.ReadLine();
         }
     }
