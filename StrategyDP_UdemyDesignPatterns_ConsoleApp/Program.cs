87b1e6de981f1920adfb9498e92ac380a689e9ae
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-17 17:28:56 +0200

    Equality and Comparation.

--- StrategyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
+++ StrategyDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy;
 using StrategyDP_UdemyDesignPatterns_ConsoleApp.DynamicAndStaticStrategy.Enums;
+using StrategyDP_UdemyDesignPatterns_ConsoleApp.EqualityAndComparation;
 using System;
+using System.Collections.Generic;
 
 namespace StrategyDP_UdemyDesignPatterns_ConsoleApp
 {
@@ -21,17 +23,24 @@ namespace StrategyDP_UdemyDesignPatterns_ConsoleApp
             #endregion
 
             #region Static Strategy
-            var tpMark = new TextProccessor<MarkdownListStrategy>();
-            tpMark.AppendList(new[] { "foo", "bar", "baz" });
-            Console.WriteLine(tpMark);
-            Console.WriteLine("-----------------------------");
-            var tpHtml = new TextProccessor<HtmlListStrategy>();
-            tpHtml.AppendList(new[] { "foo", "bar", "baz" });
-            Console.WriteLine(tpHtml);
+            //var tpMark = new TextProccessor<MarkdownListStrategy>();
+            //tpMark.AppendList(new[] { "foo", "bar", "baz" });
+            //Console.WriteLine(tpMark);
+            //Console.WriteLine("-----------------------------");
+            //var tpHtml = new TextProccessor<HtmlListStrategy>();
+            //tpHtml.AppendList(new[] { "foo", "bar", "baz" });
+            //Console.WriteLine(tpHtml);
             #endregion
 
             #region Equality and Comparation Strategies
+            var people = new List<Person>();
 
+            people.Sort();  // default strategy
+
+            //people.Sort((x,y) => x.Name.CompareTo(y.Name));  // strategy by lambda
+            people.Sort(Person.NameComparer);
+
+            // Strategy using by Dot Net Framework. 
             #endregion
 
             #region Task
