9cb5bce79c217aef221abe39dd97d37983c2e900
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-17 20:31:00 +0200

    Tamplate Method.

---
+++ TamplateMethodDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TamplateMethodDP_UdemyDesignPatterns_ConsoleApp.TemplateMethod;
+
+namespace TamplateMethodDP_UdemyDesignPatterns_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Template Method
+            var chess = new Chess();
+            chess.Run();
+            #endregion
+
+            #region Functional Template Method
+
+            #endregion
+
+            #region Task
+
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TamplateMethodDP_UdemyDesignPatterns_ConsoleApp/TemplateMethod/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TamplateMethodDP_UdemyDesignPatterns_ConsoleApp.TemplateMethod
+{
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+            while(!HaveWinner)
+                TakeTurn();
+            Console.WriteLine($"Player {WinninPlayer} wins.");
+        }
+        protected int currentPlayer;
+        protected readonly int numberOfPlayer;
+        protected Game(int numberOfPlaters)
+        {
+            this.numberOfPlayer = numberOfPlaters;
+        }
+
+        protected abstract void Start();
+        protected abstract void TakeTurn();
+        protected abstract bool HaveWinner { get; }
+        protected abstract int WinninPlayer { get; }
+    }
+}
