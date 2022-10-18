1c407c1fa122e782e29b78bc27e58f9407c873ef
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-18 15:14:56 +0200

    Visitor Design Pattern.

---
+++ VisitorDP_UdemyDesignPatterns_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisitorDP_UdemyDesignPatterns_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
