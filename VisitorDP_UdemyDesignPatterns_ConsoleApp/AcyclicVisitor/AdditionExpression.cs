b2fba88ebbffbfefa8b187a9b8eb7124ae2e28ca
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-19 16:22:23 +0200

    Acyclic Visitor.

---
+++ VisitorDP_UdemyDesignPatterns_ConsoleApp/AcyclicVisitor/AdditionExpression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VisitorDP_UdemyDesignPatterns_ConsoleApp.AcyclicVisitor.Interfaces;
+
+namespace VisitorDP_UdemyDesignPatterns_ConsoleApp.AcyclicVisitor
+{
+    public class AdditionExpression : Expression
+    {
+        public Expression Left, Right;
+        public AdditionExpression(Expression left, Expression right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override void Accept(IVisitor visitor)
+        {
+            if (visitor is IVisitor<AdditionExpression> typed)
+            {
+                typed.Visit(this);
+            }
+        }
+    }
+}
---
+++ VisitorDP_UdemyDesignPatterns_ConsoleApp/AcyclicVisitor/ExpressionPrinter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VisitorDP_UdemyDesignPatterns_ConsoleApp.AcyclicVisitor.Interfaces;
+
+namespace VisitorDP_UdemyDesignPatterns_ConsoleApp.AcyclicVisitor
+{
+    public class ExpressionPrinter : IVisitor, 
+        IVisitor<Expression>,
+        IVisitor<DoubleExpression>,
+        IVisitor<AdditionExpression>
+    {
+        private StringBuilder sb = new StringBuilder();
+        public void Visit(Expression obj)
+        {
+            
+        }
+        public void Visit(DoubleExpression obj)
+        {
+            sb.Append(obj.Value);
+        }
+        public void Visit(AdditionExpression obj)
+        {
+            sb.Append("(");
+            obj.Left.Accept(this);
+            sb.Append("+");
+            obj.Right.Accept(this);
+            sb.Append(")");
+        }
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+}
